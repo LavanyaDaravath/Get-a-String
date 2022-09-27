88eb401cea5bbd7add80798d74f4eaff11190117
LavanyaDaravath <107750127+LavanyaDaravath@users.noreply.github.com>
2022-09-27 20:56:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,13 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  let addDays = require("date-fns/addDays");
-  res.send(
-    addDays(
-      `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`,
-      100
-    )
-  );
+  res.send(`Express JS`);
 });
 app.listen(3000);
 module.exports = app;
